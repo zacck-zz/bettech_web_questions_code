fe16dfb7a04e52bc341f10600b4272c20ef53b27
Zacck <coderv63@gmail.com>
2016-04-20 06:23:58 +0000

    cell generation now occuring

--- chessboard.php
+++ chessboard.php
@@ -1,8 +1,20 @@
 <?php
-   echo '<table style="width:100%">';
-   for($rows = 0; $rows < 8; $rows++)
-   {
-        echo "<tr> row ". $rows ." </tr>";
-   }
+    //lets create a variable that will be used to determine color of a cell
+    // 0 is to make the cell blank, 1 will mean the cell is filled with color black 
+    
+    $color = 0;
+    echo '<table style="width:100%">';
+    //generate  rows for chessboard
+    for($rows = 0; $rows < 8; $rows++)
+    {
+        echo "<tr>";
+        //for each row we generate lets generate cells for that row
+        for($cells = 0; $cells < 8; $cells ++)
+        {
+            echo "<td> cell </td>";       
+                
+        }
+        echo " </tr>";
+    }
     echo '</table>';
 ?>
